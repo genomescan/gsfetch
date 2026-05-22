869a8bdd864b88f350619fb401f8f76847992870
bram <b.vanwersch@genomescan.nl>
2026-05-22 08:08:57 +0200

    proper host

--- src/variables.py
+++ src/variables.py
@@ -10,10 +10,10 @@ LOGGED_IN_URL = "login/logged-in"
 TWO_FACTOR_AUTH_URL = "login/two-factor"
 LIST_RECURSIVE = "api/projects/"
 LOGOUT_URL = "api/logout"
-HOST_URL = os.getenv("HOST_URL", "https://dev.portal.genomescan.nl")
+HOST_URL = os.getenv("HOST_URL", "https://dev.portal.genomescan.nl/")
 VERIFY_FILES_URL = "api/downloads/"
 DOWNLOAD_FILE_URL = "api/downloads/verify/"
-CA_BUNDLE = False
+CA_BUNDLE = True
 
 
 LIST_EXAMPLE_MESSAGE = """
